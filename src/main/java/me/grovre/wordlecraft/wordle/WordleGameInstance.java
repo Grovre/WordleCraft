8efbd73de4d2a64674b7115eca0ff624b5f88bb8
Grovre <50428844+Grovre@users.noreply.github.com>
2022-02-20 01:28:48 -0600

    Game progress, getting rid of GUI

--- src/main/java/me/grovre/wordlecraft/wordle/WordleGameInstance.java
+++ src/main/java/me/grovre/wordlecraft/wordle/WordleGameInstance.java
@@ -1,20 +1,36 @@
 package me.grovre.wordlecraft.wordle;
 
+import me.grovre.wordlecraft.Keys;
+import me.grovre.wordlecraft.WordleCraft;
+import org.bukkit.NamespacedKey;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.persistence.PersistentDataContainer;
+import org.bukkit.persistence.PersistentDataType;
+
+import java.util.ArrayList;
 
 public class WordleGameInstance {
 
-    private Inventory gameInv;
     private final Player player;
     private final String word;
+    private ArrayList<String> guesses;
 
     public WordleGameInstance(Player player) {
         this.player = player;
         // TODO Word will only be session word if not solved yet
         word = WordleAPI.getSessionWord();
-        gameInv = new WordleStart().startGame(player);
+        new WordleStart().startGame(player);
 
-        // TODO continue the game in steps with listeners and classes
+
+        // TODO continue the game in steps with chat interception and classes
+    }
+
+    public void setPlayerInstance(Player player, boolean isPlaying) {
+
+        PersistentDataContainer pdc = player.getPersistentDataContainer();
+        // Value is 1 if playing, 0 if not
+        pdc.set(Keys.hasInstanceKey, PersistentDataType.INTEGER, isPlaying ? 1 : 0);
+
     }
 }
--- src/main/java/me/grovre/wordlecraft/wordle/WordleStart.java
+++ src/main/java/me/grovre/wordlecraft/wordle/WordleStart.java
@@ -1,32 +1,51 @@
 package me.grovre.wordlecraft.wordle;
 
+import de.themoep.inventorygui.GuiElement;
+import de.themoep.inventorygui.InventoryGui;
+import me.grovre.wordlecraft.WordleCraft;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Monster;
 import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 
 import java.util.List;
 
 public class WordleStart {
 
-    public Inventory startGame(Player player) {
+    public void startGame(Player player) {
 
         if(isPlayerSafe(player) > 0) {
             player.sendMessage(ChatColor.RED + "You aren't safe! There are monsters nearby!");
         }
-        return createInventory();
     }
 
-    public Inventory createInventory() {
-        Inventory gameInv = Bukkit.createInventory(null, InventoryType.CHEST);
-        // TODO Fill the inventory with game-related things
-        // Specifically letters and the game
+    /*
+    Incomplete Method for gui elements
+    public Inventory createInventory(Player player) {
+        // https://docs.phoenix616.dev/inventorygui/de/themoep/inventorygui/package-summary.html
 
-        return gameInv;
+        String[] guiSetup = {
+                "  abcde  ",
+                "  fghij  ",
+                "  klmno  ",
+                "  pqrst  ",
+                "  uvwxy  ",
+                "         "
+        };
+
+        InventoryGui inv = new InventoryGui(WordleCraft.getPlugin(), player, "Wordle!", guiSetup);
+        inv.setFiller(new ItemStack(Material.CYAN_WOOL, 1));
+
+        // Incomplete
+
+        return inv;
     }
+     */
 
     public int isPlayerSafe(Player player) {
         // TODO Add radiusToCheck to config and get it from there
