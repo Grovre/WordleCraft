c218da91d068904129eadf4f85ef61c272d8b83f
Grovre <50428844+Grovre@users.noreply.github.com>
2022-02-20 00:35:58 -0600

    /wordle help

--- src/main/java/me/grovre/wordlecraft/commands/CommandUtil.java
+++ src/main/java/me/grovre/wordlecraft/commands/CommandUtil.java
@@ -2,6 +2,7 @@ package me.grovre.wordlecraft.commands;
 
 import me.grovre.wordlecraft.Permissions;
 import me.grovre.wordlecraft.wordle.WordleGameInstance;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -20,13 +21,22 @@ public class CommandUtil implements CommandExecutor {
         Player player = sender instanceof Player ? (Player) sender : null;
         updateLatest(player, sender, command, args);
 
-        if(args[0].equals("start")) {
+        if(args[0].equalsIgnoreCase("start")) {
             if(player == null) {
                 System.out.println("Command must be sent from a player!");
             } else if(!player.hasPermission(Permissions.startWordle)) {
                 Permissions.sendNoPermissionsMessage(player);
             } else {
                 new WordleGameInstance(player);
+            }
+        }
+
+        if(args[0].equalsIgnoreCase("help")) {
+            if(player != null) {
+                player.sendMessage(ChatColor.AQUA + "With Wordlecraft, you are able to play Wordle within a Minecraft GUI! " +
+                        "Just type '/wordle start' and you'll begin your game!\n" +
+                        "Once you complete the session word that resets everytime the server restarts, " +
+                        "you'll begin to receive random words");
             }
         }
 
