6a278336a29b3f30a35075fe6d86b0ed46588bf2
Grovre <50428844+Grovre@users.noreply.github.com>
2022-02-20 01:30:40 -0600

    Removing dependency implementation

--- src/main/java/me/grovre/wordlecraft/wordle/WordleStart.java
+++ src/main/java/me/grovre/wordlecraft/wordle/WordleStart.java
@@ -1,17 +1,9 @@
 package me.grovre.wordlecraft.wordle;
 
-import de.themoep.inventorygui.GuiElement;
-import de.themoep.inventorygui.InventoryGui;
-import me.grovre.wordlecraft.WordleCraft;
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.Material;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Monster;
 import org.bukkit.entity.Player;
-import org.bukkit.event.inventory.InventoryType;
-import org.bukkit.inventory.Inventory;
-import org.bukkit.inventory.ItemStack;
 
 import java.util.List;
 
@@ -23,29 +15,6 @@ public class WordleStart {
             player.sendMessage(ChatColor.RED + "You aren't safe! There are monsters nearby!");
         }
     }
-
-    /*
-    Incomplete Method for gui elements
-    public Inventory createInventory(Player player) {
-        // https://docs.phoenix616.dev/inventorygui/de/themoep/inventorygui/package-summary.html
-
-        String[] guiSetup = {
-                "  abcde  ",
-                "  fghij  ",
-                "  klmno  ",
-                "  pqrst  ",
-                "  uvwxy  ",
-                "         "
-        };
-
-        InventoryGui inv = new InventoryGui(WordleCraft.getPlugin(), player, "Wordle!", guiSetup);
-        inv.setFiller(new ItemStack(Material.CYAN_WOOL, 1));
-
-        // Incomplete
-
-        return inv;
-    }
-     */
 
     public int isPlayerSafe(Player player) {
         // TODO Add radiusToCheck to config and get it from there
