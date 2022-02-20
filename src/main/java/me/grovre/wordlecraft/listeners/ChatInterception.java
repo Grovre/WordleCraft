a5feee654c8a5208cd82923d319211a800f85f0a
Grovre <50428844+Grovre@users.noreply.github.com>
2022-02-20 13:10:21 -0600

    Chat interception progress

--- src/main/java/me/grovre/wordlecraft/listeners/ChatInterception.java
+++ src/main/java/me/grovre/wordlecraft/listeners/ChatInterception.java
@@ -1,5 +1,7 @@
 package me.grovre.wordlecraft.listeners;
 
+import me.grovre.wordlecraft.wordle.WordleAPI;
+import me.grovre.wordlecraft.wordle.WordleGameInstance;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -11,6 +13,16 @@ public class ChatInterception implements Listener {
     public void OnPlayerSendChatMessage(AsyncPlayerChatEvent event) {
 
         Player player = event.getPlayer();
+
+        WordleGameInstance gameInstance = WordleAPI.getPlayerGameInstance(player);
+        if(gameInstance == null) return;
+
+        String guess = event.getMessage();
+        if(guess.length() != 5) return;
+
+        event.setCancelled(true);
+
+        gameInstance.makeGuess(guess);
 
         // TODO Complete chat interception when the player has an active game instance
 
