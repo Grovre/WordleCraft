0f44321c1ff4adc9401f065c084fb25462abd0cd
Grovre <50428844+Grovre@users.noreply.github.com>
2022-02-19 20:27:27 -0600

    Initial commit

---
+++ src/main/java/me/grovre/wordlecraft/WordleCraft.java
@@ -0,0 +1,17 @@
+package me.grovre.wordlecraft;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class WordleCraft extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
