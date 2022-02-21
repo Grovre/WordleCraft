59e3a89d7db6343620efcc01aed2e55032ca9278
Grovre <50428844+Grovre@users.noreply.github.com>
2022-02-21 09:33:18 -0600

    redundant var

--- src/main/java/me/grovre/wordlecraft/listeners/OnPlayerJoin.java
+++ src/main/java/me/grovre/wordlecraft/listeners/OnPlayerJoin.java
@@ -1,7 +1,6 @@
 package me.grovre.wordlecraft.listeners;
 
 import me.grovre.wordlecraft.wordle.WordleAPI;
-import me.grovre.wordlecraft.wordle.WordleGameInstance;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -14,7 +13,6 @@ public class OnPlayerJoin implements Listener {
 
         // Used to double make sure that the server didn't fail to check for any game instances
         Player player = event.getPlayer();
-        WordleGameInstance gameInstance = WordleAPI.getPlayerGameInstance(player);
         WordleAPI.removeFromGameInstances(player);
     }
 
