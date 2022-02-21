f3ca02dedc1a77135dd1bb553bb739472bfdc7df
Grovre <50428844+Grovre@users.noreply.github.com>
2022-02-21 14:11:16 -0600

    win/loss count keys

--- src/main/java/me/grovre/wordlecraft/Keys.java
+++ src/main/java/me/grovre/wordlecraft/Keys.java
@@ -5,8 +5,14 @@ import org.bukkit.NamespacedKey;
 public class Keys {
 
     public static NamespacedKey hasInstanceKey;
+    // TODO implement score keys
+    public static NamespacedKey lostCountKey;
+    public static NamespacedKey winCountKey;
 
     public static void loadKeys() {
-        hasInstanceKey = new NamespacedKey(WordleCraft.getPlugin(), "hasInstance");
+        WordleCraft p = WordleCraft.getPlugin();
+        hasInstanceKey = new NamespacedKey(p, "hasInstance");
+        lostCountKey = new NamespacedKey(p, "lostCount");
+        winCountKey = new NamespacedKey(p, "winCount");
     }
 }
